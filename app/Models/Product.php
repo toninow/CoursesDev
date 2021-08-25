cd2575b67b708c31f9fb2ed1b0b0eaaeda5f2354
toninow <abenalcazarc@gmail.com>
2021-08-24 23:46:17 -0500

    model product with eloquent relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    //especificamos los campos que queremos evitar si no esta ninguna significa que obtiene todos los campos
+    //de nuestra tabla
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    //relacion uno a muchos
+    public function sizes(){
+        return $this->hasMany(Size::class);
+    }
+
+    //relacion uno a muchos inversa
+    public function brand(){
+        return $this->belongsTo(Brand::class);
+    }
+
+    //relacion uno a muchos inversa
+    public function subCategory(){
+        return $this->belongsTo(Subcategory::class);
+    }
+
+    //relacion muchos a muchos
+    public function colors(){
+        return $this->hasMany(Color::class);
+    }
 }
