4f5d9333893307af6921750871dac840c2d4c839
toninow <abenalcazarc@gmail.com>
2021-08-24 23:48:40 -0500

    model size relation with eloquent

--- app/Models/Size.php
+++ app/Models/Size.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Size extends Model
 {
     use HasFactory;
+
+    //relacion uno a muchos inversa
+    public function product(){
+        $this->belongsTo(Product::class);
+    }
+
+    //relacion muchos a muchos
+    public function colors(){
+        $this->belongsToMany(Color::class);
+    }
 }
